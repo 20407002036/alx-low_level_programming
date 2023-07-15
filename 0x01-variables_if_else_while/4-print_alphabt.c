e19374d1f6037653c032836f52c68d1ea9322bb8
solomon <kyobenimaru001@gmail.com>
2023-07-14 22:24:56 -0700

    commit for the file 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Description: the function print abcd using putchar
+* I used the for loop to acheive this
+* this program omits q and e in printing
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = 97;
+char c = (char)n;
+for (c = 97 ; c <= 122 ; c++)
+{
+if (c == 101 || c == 113)
+{c++;
+}
+else
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
