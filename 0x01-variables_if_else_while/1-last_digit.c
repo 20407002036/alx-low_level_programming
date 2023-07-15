863cb2fad91dd972c9425ff01bc32366322714ed
solomon <kyobenimaru001@gmail.com>
2023-07-14 19:36:06 -0700

    commit for the file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,11 @@
 /* more headers goes there */
 /**
 * main - Entry point
-*reaised you ca use the modulo 10 function to find the last digit of a number
-*Retrun: 0 (Success)
+* Description:This function assigns a random number to the variable `n`
+* and prints the last digit of that number along with a descriptive message.
+* reaised you ca use the modulo 10 function to find the
+*last digit of a number
+* Return: Always 0 (Success)
 */
 
 /* betty style doc for function main goes there */
@@ -24,7 +27,7 @@ if (n % 10 > 5)
 if (n % 10 == 0)
 {printf(" and is 0\n");
 }
-if (n % 10 < 6)
+if (n % 10 < 6 && n % 10 != 0)
 {printf(" and is less than 6 and not 0\n");
 }
 /* your code goes there */
