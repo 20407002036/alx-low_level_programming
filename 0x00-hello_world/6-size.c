37cf5ea830be61609595c94537881ac58b5933c9
solomon <kyobenimaru001@gmail.com>
2023-07-15 22:18:41 -0700

    commit for the file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 /**
-*main -Entry point
-*
-*Description: the function prints the size of the data types
-*This is done using the 'sizeof'
-*
-*Return: Always 0 (Success)
-*/
-int main(void)
-{printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of long int: %zu bytes\n", sizeof(int));
-printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-printf("Size of float: %zu bytes\n", sizeof(float));
-printf("Size of double: %zu bytes\n", sizeof(double));
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-return (0);
+int main(void)
+{
+  char forchar;
+  int forint;
+  long forlong;
+  long long forlonglong;
+  float forfloat;
+  printf("Size of a char: %lu byte(s)\n", sizeof(forchar));
+  printf("Size of an int: %lu byte(s)\n", sizeof(forint));
+  printf("Size of a long int: %lu byte(s)\n", sizeof(forlong));
+  printf("Size of a long long int: %lu byte(s)\n", sizeof(forlonglong));
+  printf("Size of a float: %lu byte(s)\n", sizeof(forfloat));
+  return (0);
 }
