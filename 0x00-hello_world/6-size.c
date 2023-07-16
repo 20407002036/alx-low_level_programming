8a68629df7fe3de298d15bdd26d0c2745e090649
solomon <kyobenimaru001@gmail.com>
2023-07-15 22:20:43 -0700

    commit for the file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-  char forchar;
-  int forint;
-  long forlong;
-  long long forlonglong;
-  float forfloat;
-  printf("Size of a char: %lu byte(s)\n", sizeof(forchar));
-  printf("Size of an int: %lu byte(s)\n", sizeof(forint));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(forlong));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(forlonglong));
-  printf("Size of a float: %lu byte(s)\n", sizeof(forfloat));
-  return (0);
+char forchar;
+int forint;
+long forlong;
+long long forlonglong;
+float forfloat;
+printf("Size of a char: %lu byte(s)\n", sizeof(forchar));
+printf("Size of an int: %lu byte(s)\n", sizeof(forint));
+printf("Size of a long int: %lu byte(s)\n", sizeof(forlong));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(forlonglong));
+printf("Size of a float: %lu byte(s)\n", sizeof(forfloat));
+return (0);
 }
