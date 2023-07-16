9be55b2ecdaeeb486b5c09673d6ce65243467937
solomon <kyobenimaru001@gmail.com>
2023-07-15 23:06:27 -0700

    commit for the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
-*main -Entry point
+* main - Entry Point
 *
-*Description: Prints a message without using puts,putcha or printf
-*
-*Return: Always 0 (success)
+* Return: Always 0 (Success)
 */
-int main(void) {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
-return 1;
+
+int main(void)
+{
+
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
 }
