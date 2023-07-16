cee172b43b15acc9da1fac271220c4f1707e8b61
solomon <kyobenimaru001@gmail.com>
2023-07-15 21:45:53 -0700

    commit for the file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main -Entry point of the program
+*
+*Description: The function prints to the output using puts
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char *programming = "programming is like building a multilingual puzzle" ;
+puts(programming);
+
+ return (0);
+}
