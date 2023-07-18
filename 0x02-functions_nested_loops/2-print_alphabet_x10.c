61d9687bb7d799b96b4e9b2400549918c0051822
solomon <kyobenimaru001@gmail.com>
2023-07-18 16:28:34 -0700

    commit for the file name 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
-* print_alphabet - prints the alphabet in lowercase, followed by a new line.
+* print_alphabet_x10 -will w called by main
+* Description: print_alphabet - prints the alphabet in lowercase,
+* ten times, followed by a new line.
+*
+* Return: void
 */
 void print_alphabet_x10(void)
 {
 char x;
 int y;
-for (y = 0; y < 9; y++)
+for (y = 0; y <= 9; y++)
 {
 for (x = 'a'; x <= 'z'; x++)
 {
