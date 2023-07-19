ad63bfbc104470f05bebd50bd4c21779d108c22e
solomon <kyobenimaru001@gmail.com>
2023-07-19 16:53:55 -0700

    commit for the file 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,10 @@ int main(void)
 int sum = 0;
 for (x = 0; x < 1024; x++)
 {
-if(x % 3 == 0 || x % 5 == 0)
+if (x % 3 == 0 || x % 5 == 0)
 {sum = sum + x;
 }
- }printf("%d\n",sum);
+}
+printf("%d\n", sum);
 return (0);
 }
