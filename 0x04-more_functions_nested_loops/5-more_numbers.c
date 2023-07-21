442890bf41fd08ace637749d0e8404c48dec6168
solomon <kyobenimaru001@gmail.com>
2023-07-21 16:44:35 -0700

    commit for the file 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,16 @@ void more_numbers(void)
 {
 int j;
 int i;
-for (i = 0; i < 10; i++) {
-for (j = 0; j <= 14; j++) {
-if (j >= 10) {
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
 putchar((j / 10) + '0');
 }
 putchar((j % 10) + '0');
-}putchar('\n');
+}
+putchar('\n');
 }
 }
