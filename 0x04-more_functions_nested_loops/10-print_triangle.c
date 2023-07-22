1a999e63d9b1ac8fe0dc8d8194a24ab1b036e1fa
solomon <kyobenimaru001@gmail.com>
2023-07-21 17:08:23 -0700

    commit for the file 10-triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,15 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_triangle - prints a triangle to the output
- *
- * Description: The function uses # to draw a triangle
- *
- * @size: is the size of the triangle
- *
- * Return: void
- */
+* print_triangle - prints a triangle to the output
+*
+* Description: The function uses # to draw a triangle
+*
+* @size: is the size of the triangle
+*
+* Return: void
+*/
 void print_triangle(int size)
 {
-  int i;
-  int j;
-  for (
+int i;
+int j;
+int k;
+if (size <= 0)
+{
+putchar('\n');
+}
+
+for (i = 1; i <= size; i++)
+{
+for (j = size - i; j > 0; j--)
+{
+putchar(' ');
+}
+
+for (k = 1; k <= i; k++)
+{
+putchar('#');
+}
+putchar('\n');
+}
+}
