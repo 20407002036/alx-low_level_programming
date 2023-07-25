835f5277944dcde28c9796abe85ab1a9b0596822
solomon <kyobenimaru001@gmail.com>
2023-07-24 17:18:48 -0700

    commit for the file 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -22,8 +22,8 @@ void rev_string(char *s)
 	for (i = 0; i < length / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[length -1 - i];
-		s[length -1 -i] = temp;
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
 	}
 }
 
