a432ccb02d8954f3956d8f3295838724821746ca
solomon <kyobenimaru001@gmail.com>
2023-07-26 15:27:07 -0700

    commit for the first 3 files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatinates two stings
+ *
+ * @scr: string to be added to the dest
+ * @dest: stding to be added
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+  char *dest_ptr = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (dest_ptr);
+}
---
+++ 0x06-pointers_arrays_strings/REAME.md
@@ -0,0 +1,1 @@
+This is the REAME.md file for the second project that involves pointers, arrays and strings
