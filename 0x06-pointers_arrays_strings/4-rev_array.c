a47509e59b2ffd4ec54d056be9edade8962f122a
solomon <kyobenimaru001@gmail.com>
2023-07-27 13:33:07 -0700

    commit for the file 5-string_toupper.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,24 +3,24 @@
 /**
  * reverse_array - reverses the contents of an array
  *
- * @a; pointer to the array
+ * @a: pointer to the array
  * @n: size of the array
  *
  * Return: void
  */
 void reverse_array(int *a, int n)
 {
-  int start = 0;
-  int end = n - 1;
-  int temp;
+	int start = 0;
+	int end = n - 1;
+	int temp;
 
-  while (start < end)
-    {
-      temp = a[start];
-      a[start] = a[end];
-      a[end] = temp;
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
 
-      start++;
-      end--;
-    }
+		start++;
+		end--;
+	}
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,14 +3,14 @@
 /**
  * string_toupper - changes lowercase letters to upper case
  *
- * @ :pointer to the string
+ * @str :pointer to the string
  *
  * Return: Char
  */
 char *string_toupper(char *str)
 {
-  char *ptr = str;
-  
+	char *ptr = str;
+
 	while (*ptr != '\0')
 	{
 		if (*ptr >= 'a' && *ptr <= 'z')
@@ -19,7 +19,7 @@ char *string_toupper(char *str)
 		}
 		ptr++;
 
-       }
+	}
 
 	return (str);
 }
