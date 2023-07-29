6efbdf5e1fc310910958b0402445e810b85b8a43
solomon <kyobenimaru001@gmail.com>
2023-07-28 17:55:31 -0700

    commit for the file 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ *
+ * @n: int to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
