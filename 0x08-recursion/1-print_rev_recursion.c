56a2e07c26d5f9db21b57e097170da20b5a2d226
Solomon <kyobenimaru001@gmail.com>
2023-08-02 14:54:39 -0700

    commit for the file 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pint_rev_recursion - prints a string in reverse
+ *
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+  if (*s == '\0')
+    {
+      return;
+    }
+  _print_rev_recursion(s + 1);
+  putchar(*s);
+}
