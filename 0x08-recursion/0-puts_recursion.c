ac1c3d4bd4198e9fcef99c907039ab260e599376
Solomon <kyobenimaru001@gmail.com>
2023-08-02 14:42:40 -0700

    commit for the file 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,19 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
+  if (*s == '\0')
+    {
+    putchar('\n');
+    return;
+  }
 
-	if (*s == '\0')
-	{
-		putchar('\0');
+  putchar(*s);
 
-		return;
-	}
-	else
-	{
-		putchar(*s);
-
-		_puts_recursion(s + 1);
-	}
-
-	putchar('\0');
+  _puts_recursion(s + 1);
 }
