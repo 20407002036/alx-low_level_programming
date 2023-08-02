23a86833bfc14564df749788387817a984c16e52
Solomon <kyobenimaru001@gmail.com>
2023-08-01 17:51:32 -0700

    commit for the file 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,16 +10,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-  int row;
-  int col;
-  
+
+	int row;
+	int column;
+
 	for (row = 0; row < 8; row++)
 	{
-		for (col = 0; col < 8; col++)
-		{
-			putchar(a[row][col]);
-			putchar(' ');
-		}
-		putchar('\n');
+		for (column = 0; column < 8; column++)
+			_putchar(a[row][column]);
+		_putchar('\n');
 	}
 }
