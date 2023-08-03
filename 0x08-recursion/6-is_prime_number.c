a7a2570cf2b9617642314958c7df49bc8b3f479e
Solomon <kyobenimaru001@gmail.com>
2023-08-02 17:28:52 -0700

    commit for the file 2-strlen_recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,14 +10,14 @@
 
 int is_prime_number(int prime)
 {
-  if (prime <= 1)
-    return (0);
-  if (prime == 2)
-    return (1);
-  if ((prime % 2 == 0) || (prime % 3 == 0) || (prime % 5 == 0))
-    return (0);
-  else if (prime % 9 == 0 || prime % 7 == 0)
-    return (0);
-  else
-    return (1);
+	if (prime <= 1)
+		return (0);
+	if (prime == 2)
+		return (1);
+	if ((prime % 2 == 0) || (prime % 3 == 0) || (prime % 5 == 0))
+		return (0);
+	else if (prime % 9 == 0 || prime % 7 == 0)
+		return (0);
+	else
+		return (1);
 }
