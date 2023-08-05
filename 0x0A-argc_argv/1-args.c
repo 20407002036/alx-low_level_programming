7583ffcd4f96b30f6bc15a12693d34514af040ef
Solomon <kyobenimaru001@gmail.com>
2023-08-05 13:51:48 -0700

    commit for the file 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers of arguments passed into it
+ *
+ * @argc: numbers of arguements passed to the main
+ * @argv: Pointer to the array holding the arguements to main
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+
+	if (argv[i] == 0)
+	{
+	 
+	}
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
