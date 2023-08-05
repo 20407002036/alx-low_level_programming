e7f24e9eb6510dff1eb36aa351832718c7a73fbd
Solomon <kyobenimaru001@gmail.com>
2023-08-05 12:58:17 -0700

    commit for the file and project 0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ *
+ * @argc: number of arguments from command line
+ * @argv: pointer to an array with the arguements from command line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+  if (argc >= 0)
+    {
+    }
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C- argc, argv
+
+this project focuses on the arguments given to the main function.
