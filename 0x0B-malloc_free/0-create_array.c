4b741297bfd7a825371a76a4ded0e75681573f9f
Solomon <kyobenimaru001@gmail.com>
2023-08-07 16:26:33 -0700

    commit for the file 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array _ function creates an array of char
+ *
+ * @size: size of array to be created
+ * @c: char to initialize the array
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *)malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	while(i <= size)
+	{
+		ptr[i] = c;
+
+		i++;
+	}
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,11 @@
+# 0x0B. C-malloc,free
+ The project focuses on the functions malloc and free that are used to locate memory to the program.
+ # 0. Float like a butterfly, sting like a bee
+ 
+ # 1. The woman who has no imagination has no wings
+
+ # 2. He who is not courageous enough to take risks will accomplish nothing in life
+
+ # 3. If you even dream of beating me you'd better wake up and apologize
+
+ # 4. It's not bragging if you can back it up
