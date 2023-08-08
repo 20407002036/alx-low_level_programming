57a9791a91376eca7bdcb16297fae37bab4051d9
Solomon <kyobenimaru001@gmail.com>
2023-08-08 14:23:53 -0700

    commit for the file 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,6 @@ char *str_concat(char *s1, char *s2)
 {
 	int i;
 	int j;
-	int x = 0;
 	char *ptr;
 
 	i = strlen(s1);
@@ -23,12 +22,8 @@ char *str_concat(char *s1, char *s2)
 	ptr = (char *)malloc((i + j + 1) * sizeof(char));
 
 	strcpy(ptr, s1);
+	strcat(ptr, s2);
 
-	while (x <= j)
-	{
-		ptr[(i + x)] = s2[x];
-		x++;
-	}
 
 	return (ptr);
 }
