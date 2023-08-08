e21251f1babb3c1e7af15afe93c556297589d6c5
Solomon <kyobenimaru001@gmail.com>
2023-08-08 14:27:21 -0700

    commit for the file 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,24 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 	char *ptr;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 
 	i = strlen(s1);
 	j = strlen(s2);
 
 	ptr = (char *)malloc((i + j + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return NULL;
+	}
 
 	strcpy(ptr, s1);
 	strcat(ptr, s2);
