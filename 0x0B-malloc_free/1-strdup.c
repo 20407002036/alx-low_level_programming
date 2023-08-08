ad9219b2354aa5b80a69bbcabe815ee35eb7f8a4
Solomon <kyobenimaru001@gmail.com>
2023-08-07 17:48:46 -0700

    commit for the file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function returns a pointer to a newly allocated space which
+ *  contains copy of the string given asinput
+ *
+ * @str: pointer to input string
+ *
+ * Return: char
+ */
+char *_strdup(char *str)
+{
+
+  size_t size;
+  char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	size = strlen(str);
+
+	ptr = (char *)malloc(sizeof(char) * (size + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(ptr, str);
+
+	return (ptr);
+
+}
+
+
