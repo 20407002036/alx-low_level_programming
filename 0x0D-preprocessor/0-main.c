0c57d50451586cefaa11455c3cc07f82a4d7a379
Solomon <kyobenimaru001@gmail.com>
2023-08-14 18:57:46 +0300

    commit for the file Header file macro.h

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "macro.h"
+#include "macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
