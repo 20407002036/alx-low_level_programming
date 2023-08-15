e3823acd958223f326217ed905415029932da77f
Solomon <kyobenimaru001@gmail.com>
2023-08-15 06:06:09 +0300

    commit for the file 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file it's compiled from
- *
- * Return: 0 Always (Success)
- */
+* main - prints the name of the file it's compiled from
+*
+* Return: 0 Always (Success)
+*/
 int main(void)
 {
-  printf("%s\n",__BASE_FILE__);
+printf("%s\n", __BASE_FILE__);
 
-  return (0);
+return (0);
 }
