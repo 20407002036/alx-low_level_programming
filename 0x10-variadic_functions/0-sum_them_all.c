244995d5665933dccd761304f042b15787dccbec
root <kyobenimaru001@gmail.com>
2023-08-17 12:37:55 -0700

    commit for the file 0-sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,22 +10,23 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-  int sum = 0;
-  unsigned int i;
-  va_list args;
-  if (n == 0)
-    {
-      return (0);
-    }
+	int sum = 0;
+	unsigned int i;
+	va_list args;
 
-  va_start(args, n);
+	if (n == 0)
+	{
+		return (0);
+	}
 
-  for(i = 0; i < n; i++)
-    {
-      sum = sum + va_arg(args, int);
-    }
+	va_start(args, n);
 
-  va_end(args);
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(args, int);
+	}
 
-  return (sum);
+	va_end(args);
+
+	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,28 +3,28 @@
 /**
  * print_numbers - prints the numbers given as args
  *
- * @separator: pointer to the string infront of the int 
+ * @separator: pointer to the string infront of the int
  * @n: No of int given as args
- * 
+ *
  * Return: void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    
-    va_start(args, n);
+	va_list args;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-      {
-        printf("%d", va_arg(args, int));
-        if (i < n - 1 && separator != NULL)
-	  {
-            printf("%s", separator);
-        }
-    }
+	va_start(args, n);
 
-    va_end(args);
-    printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	va_end(args);
+	printf("\n");
 }
