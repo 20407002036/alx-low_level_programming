c73fd2629cdeef94d1dd0e69f30c1aeefebaa0af
Solomon <kyobenimaru001@gmail.com>
2023-08-29 20:53:27 +0300

    commit for the file 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+* print_listint - singly linked list
+* @h: pointer ti the heaf of the list
+*
+* Description: singly linked list node structure
+*
+* Return: Always 0 (Success)
+*/
+
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+
+}
+return (0);
+}
+
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct listint_s
+{
+
+  int n;
+  struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif
