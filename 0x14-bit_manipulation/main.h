8c6945d5729f6d3a5c84d01f0c9c8a26742d09cc
Solomon <kyobenimaru001@gmail.com>
2023-09-29 16:32:08 +0300

    commit for the file 100-get_enddianess.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+int get_endianness(void);
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
